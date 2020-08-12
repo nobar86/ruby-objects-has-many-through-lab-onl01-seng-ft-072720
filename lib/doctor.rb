42c2c71c77fbf27c274e6ce2da1d273eca008921
Michael Rabon <nobar86@gmail.com>
2020-08-12 20:07:42 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,22 @@
+class Doctor
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, self, patient)
+  end
+end
